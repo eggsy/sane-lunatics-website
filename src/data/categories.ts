9853e1dc740109e389d63e54f7839ae4cc5ba5d8
Abdulbaki Dursun <eggsydev@gmail.com>
2022-04-19 17:00:31 +0300

    refactor: put people into groups

---
+++ src/data/categories.ts
@@ -0,0 +1,55 @@
+interface ICategories {
+  title: string;
+  people: string[];
+}
+
+const categories: ICategories[] = [
+  {
+    title: "Yönetmen",
+    people: ["Dr. Öğr. Üyesi Erdinç Öcal"],
+  },
+  {
+    title: "Reji Asistanı",
+    people: ["Doç. Dr. Ayça Kartal"],
+  },
+  {
+    title: "Sahne Amiri",
+    people: ["Mehmet Rıza Baba", "Adem Kaplan"],
+  },
+  {
+    title: "Müzik Yönetmeni",
+    people: ["Öğr. Gör. Zeynep Türkoğlu"],
+  },
+  {
+    title: "Işık ve Ses",
+    people: ["Batuhan Karadaş", "Abdulbaki Dursun"],
+  },
+  {
+    title: "Oyuncular",
+    people: [
+      "Dr. Öğr. Üyesi Erdinç Öcal",
+      "Doç. Dr. Ayça Kartal",
+      "Dr. Öğr. Üyesi Fatih Türkan",
+      "Dr. Öğr. Üyesi Yusuf Öztürk",
+      "Gülşah Kuru",
+      "Azra Demircan",
+      "Eda Çiçek",
+      "Mehmet Rıza Baba",
+      "Adem Kaplan",
+      "Dilan Çelik",
+      "Ercan Ateş",
+      "Handan Demir",
+      "Hüseyin Güden",
+      "İslam Ayaz",
+      "Mahfuz Ulaşkın",
+      "Melek Yeşilkaya",
+      "Mikail Artan",
+      "Selma Akdemir",
+      "Tunay Toptaş",
+      "Ünzile Günöz",
+      "Yeter Yanalak",
+    ],
+  },
+];
+
+export default categories;
