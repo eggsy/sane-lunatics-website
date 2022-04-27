faa8820aaa847c418f1af64500a6d092e3625112
Abdulbaki Dursun <eggsydev@gmail.com>
2022-04-27 22:49:49 +0300

    chore: move picture card

--- src/components/Card/Picture.tsx
+++ src/components/Card/Picture.tsx
@@ -2,7 +2,7 @@ export const PictureCard: React.FC<{
   image: string;
 }> = ({ image }) => (
   <div
-    className="w-full transition-all bg-center bg-cover rounded-lg lg:grayscale lg:hover:grayscale-0 h-44 lg:hover:scale-105"
+    className="w-full h-56 transition-all bg-center bg-cover rounded-lg lg:grayscale lg:hover:grayscale-0 lg:hover:scale-105"
     style={{ backgroundImage: `url('/previews/${image}')` }}
   />
 );
