6e838a2f30e1505686dbf669c2e763505f0040b5
Abdulbaki Dursun <eggsydev@gmail.com>
2022-04-27 22:50:11 +0300

    refactor: colors, improvements, "characters" section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,26 @@
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
-import Image from "next/image";
 
 // Types
 import type { NextPage } from "next";
 
 // Components
 import { Theater } from "../components/Icons";
-import { PictureCard } from "../components/PictureCard";
+import { PictureCard } from "../components/Card/Picture";
+import { PeopleCard } from "../components/Card/People";
 import { Title } from "../components/Title";
 
 // Data
 import categories from "../data/categories";
+import createArray from "../functions/createArray";
+
+// Static data
+const PEOPLE_AMOUNT = 21;
 
 const HomePage: NextPage = () => (
-  <Parallax pages={8} style={{ top: "0", left: "0" }}>
+  <Parallax pages={10} style={{ top: "0", left: "0" }}>
     <ParallaxLayer offset={0} speed={2.5}>
       <div className="bg-[url('/header.jpg')] px-6 lg:px-0 w-full h-full bg-cover bg-center flex items-center justify-center">
-        <h1 className="text-6xl font-black text-center text-white lg:text-8xl lg:w-1/2">
+        <h1 className="text-6xl font-black text-center text-white drop-shadow-lg lg:text-8xl lg:w-1/2">
           Aklı Başında Deliler Kumpanyası
         </h1>
       </div>
@@ -43,20 +47,18 @@ const HomePage: NextPage = () => (
     <ParallaxLayer
       offset={2}
       speed={0.5}
-      className="flex items-center justify-center px-6 lg:px-0"
+      className="flex items-center justify-center px-6 bg-white/5 lg:px-0"
     >
-      <div className="w-full lg:w-2/3">
-        <div className="space-y-14">
-          <Title>Önizleme</Title>
+      <div className="w-full lg:w-2/3 space-y-14">
+        <Title>Önizleme</Title>
 
-          <div className="grid w-full gap-6 lg:grid-cols-3">
-            <PictureCard image="/1.jpg" />
-            <PictureCard image="/2.jpeg" />
-            <PictureCard image="/3.jpeg" />
-            <PictureCard image="/4.jpeg" />
-            <PictureCard image="/5.jpeg" />
-            <PictureCard image="/6.jpeg" />
-          </div>
+        <div className="grid w-full gap-6 lg:grid-cols-3">
+          <PictureCard image="/1.jpg" />
+          <PictureCard image="/2.jpeg" />
+          <PictureCard image="/3.jpeg" />
+          <PictureCard image="/4.jpg" />
+          <PictureCard image="/5.jpeg" />
+          <PictureCard image="/6.jpg" />
         </div>
       </div>
     </ParallaxLayer>
@@ -66,15 +68,15 @@ const HomePage: NextPage = () => (
       speed={0.5}
       className="flex items-center justify-center px-6 lg:px-0"
     >
-      <div className="space-y-4 text-6xl text-center ">
+      <div className="space-y-4 text-center">
         <div>
           <span className="text-2xl text-white/50">yer ve zaman</span>
-          <h3 className="animate-pulse">25 Mayıs 2022</h3>
+          <h3 className="animate-pulse text-8xl">25 Mayıs 2022</h3>
         </div>
 
         <div className="flex flex-col leading-tight">
-          <span className="text-3xl leading-tight lg:text-2xl">
-            2071 Kongre Merkezi
+          <span className="text-3xl leading-tight opacity-75 lg:text-2xl">
+            1071 Kongre Merkezi
           </span>
 
           <span className="text-xl leading-tight lg:text-base text-white/50">
@@ -87,25 +89,24 @@ const HomePage: NextPage = () => (
     <ParallaxLayer
       offset={4}
       speed={0.5}
-      className="flex items-center justify-center px-6 lg:px-0"
+      factor={2}
+      className="flex items-center justify-center px-6 bg-white/5 lg:px-0"
     >
-      <div>
-        <div className="mx-auto space-y-14 lg:w-1/3 ">
-          <Title>Ekip</Title>
+      <div className="w-full mx-auto space-y-14 lg:w-2/3 ">
+        <Title>Ekip</Title>
 
-          <div className="space-y-10">
-            {categories.map((category, index) => (
-              <div key={`category-${index}`} className="space-y-2 ">
-                <h3 className="text-3xl border-b text-white/80 border-white/10">
-                  {category.title}
-                </h3>
+        <div className="grid gap-10 lg:grid-cols-2">
+          {categories.map((category, index) => (
+            <div key={`category-${index}`} className="space-y-2 ">
+              <h3 className="text-3xl border-b w-max text-white/80 border-white/20">
+                {category.title}
+              </h3>
 
-                <div className="text-2xl text-white/50">
-                  {category.people.join(", ")}
-                </div>
+              <div className="text-2xl text-white/50">
+                {category.people.join(", ")}
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </ParallaxLayer>
@@ -113,55 +114,50 @@ const HomePage: NextPage = () => (
     <ParallaxLayer
       offset={5}
       speed={0.5}
-      factor={1.5}
+      factor={4}
       className="flex items-center justify-center px-6 lg:px-0"
     >
-      <div className="mx-auto space-y-10 lg:w-2/4">
-        <Title>Yönetmenin Görüşü</Title>
+      <div className="w-full space-y-14 lg:w-2/3">
+        <Title>Karakterler</Title>
 
-        <div className="flex flex-col justify-center lg:items-center lg:flex-row gap-x-8 gap-y-4">
-          <div className="flex-shrink-0 ">
-            <Image
-              src="/erdinc-ocal.jpeg"
-              alt="Erdinç Öcal profil resmi"
-              className="transition-all rounded-lg filter blur-sm hover:blur-0 w-36 h-44"
-              width="144"
-              height="176"
-            />
-          </div>
-
-          <p className="text-2xl text-white/50">
-            Valla bence her şey çok güzel. Harika bir gösteri olacak. Görülmedik
-            bir şey olacak. Çok komik olacak. Güleceğiz. Canım öğrencilerimle
-            başaracağız bu oyunu. Zamanında çıkartacağız. Hazırlanıyoruz.
-          </p>
+        <div className="grid w-full grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6">
+          {createArray(PEOPLE_AMOUNT).map((_, index) => (
+            <PeopleCard key={`people-${index}`} image={`/${index + 1}.jpg`} />
+          ))}
         </div>
       </div>
     </ParallaxLayer>
 
     <ParallaxLayer
-      offset={6}
+      offset={8}
       speed={0.5}
-      className="flex items-center justify-center"
+      className="flex items-center justify-center px-6 bg-white/5 lg:px-0"
     >
-      <Title>Herkes Davetlidir!</Title>
+      <div className="mx-auto space-y-10 lg:w-2/4">
+        <Title>Yönetmenin Görüşü</Title>
+
+        <p className="text-2xl text-center text-white/50">
+          Valla bence her şey çok güzel. Harika bir gösteri olacak. Görülmedik
+          bir şey olacak. Çok komik olacak. Güleceğiz. Canım öğrencilerimle
+          başaracağız bu oyunu. Zamanında çıkartacağız. Hazırlanıyoruz.
+        </p>
+      </div>
     </ParallaxLayer>
 
     <ParallaxLayer
-      offset={7}
+      offset={9}
       speed={0.5}
       className="flex items-center justify-center"
     >
-      <div>
-        <a
-          href="https://eggsy.xyz/?utm_source=sane-lunatics-website"
-          rel="noreferrer"
-          className="text-2xl transition-colors border-b border-dashed lg:text-4xl hover:border-white border-white/50 text-white/50 hover:text-white"
-          lang="en"
-        >
-          a website by EGGSY
-        </a>
-      </div>
+      <a
+        href="https://eggsy.xyz/?utm_source=sane-lunatics-website"
+        rel="noreferrer"
+        target="_blank"
+        className="text-4xl transition-colors border-b border-dashed hover:border-white border-white/50 text-white/50 hover:text-white"
+        lang="en"
+      >
+        a website by EGGSY
+      </a>
     </ParallaxLayer>
   </Parallax>
 );
