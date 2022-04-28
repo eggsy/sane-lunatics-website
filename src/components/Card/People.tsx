6d41adf6757d6f4fed77ed8e114c10e52396f932
Abdulbaki Dursun <eggsydev@gmail.com>
2022-04-28 13:07:49 +0300

    feat: role and name section in people cards

--- src/components/Card/People.tsx
+++ src/components/Card/People.tsx
@@ -2,14 +2,32 @@ import Image from "next/image";
 
 export const PeopleCard: React.FC<{
   image: string;
-}> = ({ image }) => (
-  <Image
-    src={`/people${image}`}
-    height="200px"
-    width="100"
-    objectFit="cover"
-    alt="person image"
-    className="transition-all transform rounded-xl lg:hover:scale-105 lg:grayscale filter lg:hover:grayscale-0"
-  />
-);
+  name?: string;
+  role?: string;
+}> = ({ image, name, role }) => {
+  const PersonImage = (
+    <Image
+      src={`/people${image}`}
+      height="200px"
+      width="100"
+      objectFit="cover"
+      alt="person image"
+      className="transition-all transform rounded-xl lg:hover:scale-105 lg:grayscale filter lg:hover:grayscale-0"
+    />
+  );
+
+  if (name && role)
+    return (
+      <div className="flex flex-col w-full space-y-2">
+        {PersonImage}
+
+        <div className="flex flex-col items-center flex-shrink-0">
+          <span className="text-xl opacity-75">{name}</span>
+          <span className="opacity-50">{role}</span>
+        </div>
+      </div>
+    );
+
+  return PersonImage;
+};
 export default PeopleCard;
