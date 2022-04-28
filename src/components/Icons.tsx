1e0fdb8b4d35729b7b46f1fe954cb94a82a92d42
Abdulbaki Dursun <eggsydev@gmail.com>
2022-04-28 13:07:37 +0300

    refactor: change mail icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -53,7 +53,7 @@ export const Location: React.FC = () => (
   </svg>
 );
 
-export const Phone: React.FC = () => (
+export const Mail: React.FC = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     className="w-6 h-6"
@@ -65,7 +65,7 @@ export const Phone: React.FC = () => (
     <path
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+      d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
     />
   </svg>
 );
