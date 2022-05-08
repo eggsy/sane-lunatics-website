84e2baa01cc1c72225946c4fa74984cc22ffb8a7
Abdulbaki Dursun <eggsydev@gmail.com>
2022-05-08 21:48:32 +0300

    style: fix mobile overflow issue

--- src/pages/galeri.tsx
+++ src/pages/galeri.tsx
@@ -20,7 +20,7 @@ const GalleryPage: NextPage = () => {
   );
 
   const baseButtonClasses =
-    "px-6 py-4 font-bold uppercase transition-colors hover:bg-yellow-500";
+    "px-4 py-2 lg:px-6 lg:py-4 font-bold uppercase transition-colors hover:bg-yellow-500";
 
   // Memo
   const getColsAmount = useMemo(() => {
