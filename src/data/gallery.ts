0bd8f18404a5012cb4308d1a7835b2f61daf3907
Abdulbaki Dursun <eggsydev@gmail.com>
2022-05-08 18:12:53 +0300

    feat: gallery data

---
+++ src/data/gallery.ts
@@ -0,0 +1,44 @@
+interface VideoItem {
+  poster: string;
+  videoId: string;
+}
+
+interface GalleryItems {
+  images: string[];
+  videos: VideoItem[];
+}
+
+const images: string[] = [
+  "/previews/1.jpg",
+  "/previews/2.jpg",
+  "/previews/3.jpg",
+  "/previews/4.jpg",
+  "/previews/5.jpg",
+  "/previews/6.jpg",
+  "https://i.imgur.com/RylvfA4.jpg",
+  "https://i.imgur.com/t8zOLDW.jpg",
+  "https://i.imgur.com/qACtImr.jpg",
+  "https://i.imgur.com/Gp0Sq2i.jpg",
+];
+
+const videos: VideoItem[] = [
+  {
+    poster: "https://i.imgur.com/Uun3418.jpg",
+    videoId: "707504815",
+  },
+  {
+    poster: "https://i.imgur.com/un0mbBH.jpg",
+    videoId: "707504529",
+  },
+  {
+    poster: "https://i.imgur.com/Jx9A6fd.jpg",
+    videoId: "707503958",
+  },
+];
+
+export const gallery: GalleryItems = {
+  images,
+  videos,
+};
+
+export default gallery;
