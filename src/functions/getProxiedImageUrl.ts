21674f55ee08764defe7a6a9b3feefef791296b6
Abdulbaki Dursun <eggsydev@gmail.com>
2022-05-10 18:45:57 +0300

    refactor: encode URI

--- src/functions/getProxiedImageUrl.ts
+++ src/functions/getProxiedImageUrl.ts
@@ -1,7 +1,7 @@
 export const getProxiedImageUrl = (url: string) => {
   try {
     new URL(url);
-    return `https://proxy.duckduckgo.com/iu/?u=${url}`;
+    return `https://proxy.duckduckgo.com/iu/?u=${encodeURIComponent(url)}`;
   } catch (err) {
     return url;
   }
