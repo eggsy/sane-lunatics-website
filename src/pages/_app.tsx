ae9bc1cde688aa17da50a87c5a2d380c1dd1d372
Abdulbaki Dursun <eggsydev@gmail.com>
2022-05-14 17:24:21 +0300

    feat: replace campaign parameter with utm_campaign

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Router from "next/router";
+import { useEffect } from "react";
 
 // Modules
 import NProgress from "nprogress";
@@ -27,36 +28,58 @@ Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done(true));
 Router.events.on("routeChangeError", () => NProgress.done(true));
 
-const SaneLunaticsWebsite = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <title>{meta.title}</title>
-      <meta name="description" content={meta.description} />
-      <meta name="theme-color" content="#000000" />
-      <link rel="icon" href="/favicon.png" />
+const SaneLunaticsWebsite = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if (typeof window === "undefined") return;
 
-      <meta name="author" content="Abdulbaki 'EGGSY' Dursun" />
-      <meta
-        name="keywords"
-        content="Tiyatro, Muş, Aklı Başında Deliler, Komedi, Eğlence, Şov"
-      />
+    const url = new URL(window.location.href);
+    const isCampaign = url.searchParams.get("campaign");
 
-      {/* Open-Graph */}
-      <meta property="og:title" content={meta.title} />
-      <meta property="og:description" content={meta.description} />
+    /*
+      I created  QR codes to stick on posters and accidentally
+      used "campaign" parameter instead of "utm_campaign" so I will
+      redirect users who come from the QR campaign to have the correct
+      parameter.
+    */
+    if (isCampaign) {
+      url.searchParams.delete("campaign");
+      url.searchParams.set("utm_campaign", isCampaign);
 
-      {/* Twitter */}
-      <meta name="twitter:creator" content="@eggsydev" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={meta.title} />
-      <meta name="twitter:description" content={meta.description} />
-      <meta name="twitter:image" content={meta.image} />
-    </Head>
+      window.location.href = url.toString();
+    }
+  }, []);
 
-    <div className="min-h-screen text-white bg-black font-amatic ">
-      <Component {...pageProps} />
-    </div>
-  </>
-);
+  return (
+    <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="description" content={meta.description} />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="/favicon.png" />
+
+        <meta name="author" content="Abdulbaki 'EGGSY' Dursun" />
+        <meta
+          name="keywords"
+          content="Tiyatro, Muş, Aklı Başında Deliler, Komedi, Eğlence, Şov"
+        />
+
+        {/* Open-Graph */}
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+
+        {/* Twitter */}
+        <meta name="twitter:creator" content="@eggsydev" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+      </Head>
+
+      <div className="min-h-screen text-white bg-black font-amatic ">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
+};
 
 export default SaneLunaticsWebsite;
