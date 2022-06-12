7a33b2dde0ec9f11c1aff98988b1ea9e3e655ba4
Abdulbaki Dursun <eggsydev@gmail.com>
2022-06-12 16:22:10 +0300

    chore: add new images and a video

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -18,8 +18,12 @@ const images = [
   "https://i.imgur.com/Li07W77.jpg",
   "https://i.imgur.com/Mc7wrPr.jpg",
   "https://i.imgur.com/IoAPaMV.jpg",
+  "https://i.imgur.com/KmrcIs6.jpg",
+  "https://i.imgur.com/ijtiHk2.jpg",
+  "https://i.imgur.com/j8HFASU.jpg",
   "https://i.imgur.com/Zzi0zd5.jpg",
   "https://i.imgur.com/FfP7SGG.jpg",
+  "https://i.imgur.com/IDa5PRg.jpg",
   "https://i.imgur.com/qxlEfPz.jpg",
   "https://i.imgur.com/n2LjSBS.jpg",
   "https://i.imgur.com/mwDa0HW.jpg",
@@ -35,20 +39,24 @@ const posters = [
 
 const videos: VideoItem[] = [
   {
-    poster: "https://i.imgur.com/Uun3418.jpg",
-    videoId: "707504815",
+    poster: "https://i.imgur.com/cf8wHQk.jpg",
+    videoId: "719561631",
   },
   {
-    poster: "https://i.imgur.com/un0mbBH.jpg",
-    videoId: "707504529",
+    poster: "https://i.imgur.com/DKtwEbh.jpg",
+    videoId: "707516501",
   },
   {
     poster: "https://i.imgur.com/Jx9A6fd.jpg",
     videoId: "707503958",
   },
   {
-    poster: "https://i.imgur.com/DKtwEbh.jpg",
-    videoId: "707516501",
+    poster: "https://i.imgur.com/un0mbBH.jpg",
+    videoId: "707504529",
+  },
+  {
+    poster: "https://i.imgur.com/Uun3418.jpg",
+    videoId: "707504815",
   },
 ];
 
